a4be72254930e41eb8f5827dd141586a525dacc7
mtkranmini <135610219+mtkranmini@users.noreply.github.com>
2024-09-17 14:52:37 +0530

    hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
 
 function hero() {
   return (
-    <div>hero</div>
+    <div className="p-60">
+      <h1 className="text-6xl font-semibold leading normal" >Building digital<br></br>product & brands.	</h1><br></br>
+<p>Here at flowbite we focus on markets where technology,innovation, and <br></br>capital can 
+unlock long-term value and drive economic growth. </p>
+
+<div className="p-8 mt-10 space-x-6 ">
+  <a href="#" className="inline px-8 py-3 font-medium text-white bg-purple-700 rounded-2xl duration 300">Start 30 day free trial</a>
+  <a href="#" className="inline gap-3 px-8 py-3 font-medium border-2 bg-slate-50 rounded-2xl">Pricing & FAQ</a>
+
+</div>
+<img src="src/assets/marketing-strategy.png" className="relative bottom-0 w-full p-100 px-28 py-28 xl:w-1/2 xl:absolute right-1 top-20"></img>
+
+    </div>
   )
 }
 
