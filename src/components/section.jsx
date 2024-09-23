f3d3a4d5faa56cc0c1b6ffe5931d215b656c9b3c
nethmibandara331 <kavindi.bandara@gmail.com>
2024-09-23 20:38:55 +0530

    resolve section

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -2,13 +2,12 @@ import { HiCheckCircle } from "react-icons/hi";
 
 function section() {
   return (
-    <div className="flex flex-row gap-10 p-36">
+    <div className="flex flex-col items-center justify-between gap-10 md:flex-row">
       
-      <div>
+      <div className="md:w-1/2">
       <h1 className="font-extrabold text-gray-900 text-[2rem] mb-4">Work with tools you already use</h1>
       <div className="mb-8">
-        <p className="text-xl font-normal text-gray-500 ">Deliver great service experiences fast - without the complexity <br /> 
-        of traditional ITSM solutions.Accelerate critical development <br /> 
+        <p className="text-xl font-normal text-gray-500 ">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development 
         work, eliminate toil, and deploy changes with ease.</p>
         </div>
         <div className="mb-8">
@@ -28,12 +27,12 @@ function section() {
         <p className="text-base font-medium text-gray-900">Knowledge management</p>
         </div>
         <div className="my-6">
-        <p className="text-xl font-normal text-gray-500">Deliver great service experiences fast - without the complexity <br /> of traditional ITSM solutions.</p>
+        <p className="text-xl font-normal text-gray-500">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.</p>
         </div>
       </div>
         
-        <div>
-        <img src="src/assets/features-1 1.png" className=""></img>
+        <div className="md:w-1/2"> 
+        <img src="src/assets/features-1 1.png" className="object-cover w-full h-auto"></img>
         </div>
        
         
