d388014864d0b4314a22784491475649d611d60d
mtkranmini <135610219+mtkranmini@users.noreply.github.com>
2024-09-24 00:00:25 +0530

    done start

---
+++ src/components/start.jsx
@@ -0,0 +1,17 @@
+function Start() {
+    return (
+      <div className="items-center justify-center gap-6 mx-10 mb-6 lg:flex-row lg:items-center lg:justify-between lg:gap-20">
+        <h1 className="text-2xl font-bold text-center">Start your trial today</h1><br></br>
+        <p className="text-center">
+          Try Flowbite Platform for 30 days. No credit card required.
+        </p><br></br>
+        <div className="flex justify-center">
+        <a href="#"
+          className="inline px-6 py-2 font-medium text-center border-2 bg-primary rounded-2xl"
+        >Free trial for 30 days</a></div>
+      </div>
+    );
+  }
+  
+  export default Start;
+  
